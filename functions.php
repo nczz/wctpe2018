dede7e2020c723f188c4035d0be5d6dd3d18cb13
Chiang Hung Chun <im@mxp.tw>
2018-10-09 02:27:34 +0800

    update

--- functions.php
+++ functions.php
@@ -156,7 +156,7 @@ function wctpe2018_form_shortcode($atts) {
 					'post_content' => '[wctpe2018_display id="' . $pid . '"]',
 				);
 				wp_update_post($update_post);
-				return '<script>alert("Thank you! Now redirect to your post.");setTimeout(function(){location.href="' . get_post_permalink($pid) . '";},500);</script>';
+				$content .= '<script>alert("Thank you! Now redirect to your post.");setTimeout(function(){location.href="' . get_post_permalink($pid) . '";},500);</script>';
 				// if (wp_redirect(get_post_permalink($pid))) {
 				// 	exit;
 				// }
