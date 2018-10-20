dbab27440ee39a18696b64a6ae76fbaef289725a
Chiang Hung Chun <im@mxp.tw>
2018-10-21 01:09:55 +0800

    update

--- functions.php
+++ functions.php
@@ -380,3 +380,19 @@ function wctpe2018_get_searchform_shortcode($atts) {
 }
 
 add_shortcode('wctpe2018_get_searchform', 'wctpe2018_get_searchform_shortcode');
+
+function wctpe2018_typekit() {
+	?>
+	<script>
+  (function(d) {
+    var config = {
+      kitId: 'uas7eyf',
+      scriptTimeout: 3000,
+      async: true
+    },
+    h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+  })(document);
+</script>
+	<?php
+}
+add_action('wp_head', 'wctpe2018_typekit');
