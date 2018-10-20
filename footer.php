06f4dcb560ba7396885806ca9903449d5e8b9195
Chiang Hung Chun <im@mxp.tw>
2018-10-21 01:05:04 +0800

    update

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<footer  class="footer text-center">WCTPE 2018 Campaign site Made By Proud WordPress Developer: <a href="https://www.mxp.tw">Chun</a>, <a href="https://piglife.tw/">Terry</a></footer>
+<footer  class="footer text-center">WCTPE 2018 Campaign site Made By Proud WordPress Developer: <a href="https://www.mxp.tw">Chun</a>, <a href="https://piglife.tw/">Terry</a>, <a href="https://yuliyang.com/">Yuli</a></footer>
 </body>
 <?php wp_footer();?>
 </html>
