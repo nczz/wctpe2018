97d8f28462abbbc3c23377ebebfb28ea291c906a
Chiang Hung Chun <im@mxp.tw>
2018-10-21 00:50:43 +0800

    update

--- functions.php
+++ functions.php
@@ -361,3 +361,22 @@ function mxp_wctpe2018_form_processing() {
 	}
 }
 add_action('init', 'mxp_wctpe2018_form_processing');
+
+function wctpe2018_search_form($form) {
+	$form = '<div class="searchform find_post"><form role="search" method="get" id="searchform" class="searchform" action="' . home_url('/') . '" >
+    <div><label class="screen-reader-text" for="s">' . __('Search for:') . '</label>
+    <input type="text" value="' . get_search_query() . '" name="s" id="s" />
+    <input type="submit" id="searchsubmit" value="' . esc_attr__('Search') . '" />
+    </div>
+    </form>';
+
+	return $form;
+}
+add_filter('get_search_form', 'wctpe2018_search_form');
+
+function wctpe2018_get_searchform_shortcode($atts) {
+	$form = get_search_form();
+	return $form;
+}
+
+add_shortcode('wctpe2018_get_searchform', 'wctpe2018_get_searchform_shortcode');
